e44390c0a3e3225070b8dbd388a9ec5ab07a3851
zahid-rahman <rahmanzahid94@gmail.com>
2023-01-01 20:22:47 +0600

    feat: add todo crud api

--- api/todoproject/todoproject/settings.py
+++ api/todoproject/todoproject/settings.py
@@ -78,10 +78,19 @@ WSGI_APPLICATION = 'todoproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'todo_db',
+        'HOST': '127.0.0.1',
+        'PORT': 27017,
     }
 }
 
