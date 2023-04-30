45448fe930561e8b45932647a9626c836d3c24db
zahid-rahman <rahmanzahid94@gmail.com>
2023-04-30 14:09:18 +0600

    feat: add server side rendering in todo list page

--- rtk-app/pages/index.tsx
+++ rtk-app/pages/index.tsx
@@ -1,10 +1,96 @@
-import Head from 'next/head'
-import { Inter } from 'next/font/google'
-import Frontpage from '@/components/frontpage/Frontpage'
+import { todoApi, useAddTodoMutation, useDeleteTodoMutation, useTodoQuery, useTodosQuery } from '@/services/todoApi'
+import { useFormik } from 'formik';
+import React, { useEffect, useState } from 'react'
+import { Input, Button, Space, Table, Tag } from 'antd';
+import { } from 'antd';
+import type { ColumnsType } from 'antd/es/table';
+import { notifyError, notifySuccess } from '@/ui';
+import AppModal from '@/ui/Modal';
+import Link from 'next/link';
+import Head from 'next/head';
+import { store } from '@/store';
+import { useRouter } from 'next/router';
 
-const inter = Inter({ subsets: ['latin'] })
+function Frontpage({ data: todos }: any) {
+  const router = useRouter();
+  const [addTodo] = useAddTodoMutation();
+  const [deleteTodo] = useDeleteTodoMutation()
+  const [open, setOpen] = useState<boolean>(false);
+  const [selectedId, setSelectedId] = useState<string>("");
 
-export default function Home() {
+  const { data = todos } = useTodosQuery();
+
+
+  const columns: ColumnsType<any> = [
+    {
+      title: 'Task',
+      dataIndex: 'task',
+      key: 'task',
+      render: (text) => <a>{text}</a>,
+    },
+    {
+      title: 'Done',
+      dataIndex: 'done',
+      key: 'done',
+      render: (done) => {
+        return (
+          <>{done === true ? "Yes" : "No"}</>
+        )
+      }
+    },
+    {
+      title: 'Created at',
+      dataIndex: 'createdAt',
+      key: 'createdAt',
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      render: (_, record) => (
+        <Space size="middle">
+          <Link href={`/todo/${record._id}`}>
+            <Button type="primary">edit</Button>
+          </Link>
+          <Button type="primary" danger onClick={() => {
+            setOpen(true)
+            setSelectedId(record._id)
+          }}>delete</Button>
+        </Space>
+      ),
+    },
+  ];
+
+  const { handleSubmit, handleChange, handleBlur, values } = useFormik({
+    initialValues: {
+      task: "",
+      done: false,
+    },
+    async onSubmit(values, action) {
+      try {
+        await addTodo(values).unwrap();
+        notifySuccess("todo added successfully");
+        action.resetForm();
+      }
+      catch (error) {
+        console.error(error);
+        notifyError("something went wrong")
+      }
+    }
+  });
+
+  const deleteTodoHandler = async (id: string) => {
+    try {
+      await deleteTodo(id);
+      notifySuccess('todo deleted successfully');
+    }
+    catch (error) {
+      console.error(error);
+      notifyError('something went wrong');
+    }
+  }
+
+  // if (isLoading) return null;
+
   return (
     <>
       <Head>
@@ -13,7 +99,51 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Frontpage />
+      <div style={{
+        margin: "0px 0px 50px 0px"
+      }}>
+        <form onSubmit={handleSubmit}>
+          <Input placeholder="Basic usage" name="task" onChange={handleChange} onBlur={handleBlur} value={values.task} />
+          <Button htmlType="submit" type="primary" style={{
+            margin: "10px 0px 0px 0px"
+
+          }}>Add todo</Button>
+        </form>
+      </div>
+
+      <Table columns={columns} dataSource={data?.data} />
+
+      <AppModal
+        open={open}
+        setOpen={setOpen}
+        title="Delete todo"
+        action={() => deleteTodoHandler(selectedId)}
+        onCancel={() => {
+          setSelectedId("")
+        }}
+      >
+        <p>want to delete todo?</p>
+      </AppModal>
     </>
   )
+}
+
+export default Frontpage;
+
+export async function getServerSideProps(ctx: any) {
+  // const { res } = ctx;
+  // res.setHeader(
+  //   'Cache-Control',
+  //   'public, s-maxage=10, stale-while-revalidate=59'
+  // )
+  const action = todoApi.endpoints.todos.initiate();
+  const result = await store.dispatch(action);
+  const postsState = todoApi.endpoints.todos.select()(store.getState());
+  const todos = JSON.parse(JSON.stringify(postsState));
+
+  return {
+    props: {
+      data: todos.data
+    },
+  }
 }
--- rtk-app/store/index.ts
+++ rtk-app/store/index.ts
@@ -1,12 +1,18 @@
 import { todoApi } from "@/services/todoApi";
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 
+const rootReducer = combineReducers({
+    todoApi: todoApi.reducer,
+  })
 
-export const store = configureStore({
-    reducer: {
-        // Add the generated reducer as a specific top-level slice
-        [todoApi.reducerPath]: todoApi.reducer
-    },
+export type RootState = ReturnType<typeof rootReducer>;
+
+export const store: any = configureStore({
+    // reducer: {
+    //     // Add the generated reducer as a specific top-level slice
+    //     [todoApi.reducerPath]: todoApi.reducer
+    // },
+    reducer: rootReducer,
     // Adding the api middleware enables caching, invalidation, polling,
     // and other useful features of `rtk-query`.
     middleware: (getDefaultMiddleware) =>
